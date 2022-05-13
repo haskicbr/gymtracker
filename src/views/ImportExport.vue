8a2783b9157fa12405f173ae23a9493974a972d4
Vladislav Aleynik <haskicbr@gmail.com>
2022-05-13 07:25:55 +0300

    Fix download encode

--- src/views/ImportExport.vue
+++ src/views/ImportExport.vue
@@ -63,7 +63,7 @@ export default defineComponent({
       const response = await ConvertStateService.convertToCSV(this.$store.state);
       const csv = response.data as BlobPart;
       const a = window.document.createElement('a');
-      a.href = window.URL.createObjectURL(new Blob([csv], { type: 'text/csv' }));
+      a.href = window.URL.createObjectURL(new Blob([csv], { type: 'text/csv;charset=utf8' }));
       a.download = 'План тренировок.csv';
       document.body.appendChild(a);
       a.click();
