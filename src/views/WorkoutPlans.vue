3e6628b012da436cec25d64c6c745c35446687cf
Vladislav <haskicbr@gmail.com>
2022-05-16 21:21:56 +0300

    workoutPlans sort

--- src/views/WorkoutPlans.vue
+++ src/views/WorkoutPlans.vue
@@ -14,7 +14,20 @@ export default defineComponent({
     WorkoutPlanListView
   },
   data() {
-    const workoutPlans = this.$store.state.workoutPlans;
+
+    const workoutPlans =  this.$store.state.workoutPlans.sort((a,b) => {
+      const titleA = a.title.toLowerCase();
+      const titleB = b.title.toLowerCase();
+
+      if (titleA < titleB ){
+        return -1;
+      }
+      if ( titleA > titleB ){
+        return 1;
+      }
+      return 0;
+    });
+
     return {
       workoutPlans
     }
